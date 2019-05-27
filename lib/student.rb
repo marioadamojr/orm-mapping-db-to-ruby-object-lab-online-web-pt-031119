d96261065225ca6dae405504fc4c9819c7d99847
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-27 21:18:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,7 +105,7 @@ class Student
       sql = <<-SQL
         SELECT *
         FROM students
-        WHEHRE students.grade == 10
+        WHERE students.grade == 10
         LIMIT 1
       SQL
       DB[:conn].execute(sql)

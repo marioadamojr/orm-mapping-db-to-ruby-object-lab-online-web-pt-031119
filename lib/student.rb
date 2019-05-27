ecfdf38be39563a8cd3cd1fcd1a554bb91de32a6
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-27 21:08:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,8 @@ class Student
         FROM students
         WHERE students.grade == 9
         SQL
-      DB[:conn].execute(sql)
+      DB[:conn].execute(sql).collect do |row|
+        self.new_from_db(row)
     end
 
     def self.students_below_12th_grade
